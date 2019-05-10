9dc0e9e3740352c3c6c5430f926a1e5fdd33f295
jaegeon <zezaeoh@gmail.com>
2019-05-10 13:37:09 +0900

    1st release

--- src/components/overview/CountOverview.js
+++ src/components/overview/CountOverview.js
@@ -7,21 +7,37 @@ import {
 import "../css/Overview.css"
 
 
-const CountOverview = ({ cnt, cur_status }) => (
-  <Card>
-    <CardBody>
-      <div className="overview">
-        <div className="overview__content">
-          <h2 className="text-primary">5<b>명</b></h2>
-          <h3>현재는 <span>안전한 상태</span>입니다!</h3>
-          <img
-            src={require("./../../images/content/overview_img.svg")}
-            alt="main desc"
-          />
+const CountOverview = ({ cnt, dr_status }) => {
+  let stat, cn;
+  if(dr_status === -1){
+    stat = '운행 중 ';
+    cn = 'okay';
+  }else if(dr_status === 0){
+    stat = '정상 운행 종료';
+    cn = 'okay';
+  }else if(dr_status === 1){
+    stat = '비상 1단계';
+    cn = 'emerg';
+  }else if(dr_status === 2){
+    stat = '비상 2단계';
+    cn = 'emerg';
+  }
+  return (
+    <Card>
+      <CardBody>
+        <div className="overview">
+          <div className="overview__content">
+            <h2 className="text-primary">{cnt}<b>명</b></h2>
+            <h3>현재 <span className={cn}>{stat}</span>입니다!</h3>
+            <img
+              src={require("./../../images/content/overview_img.svg")}
+              alt="main desc"
+            />
+          </div>
         </div>
-      </div>
-    </CardBody>
-  </Card>
-);
+     </CardBody>
+    </Card>
+  );
+};
 
 export default CountOverview;
--- src/views/Overview.js
+++ src/views/Overview.js
@@ -1,29 +1,63 @@
-import React from "react";
-import { Container, Row, Col } from "shards-react";
+import React from "react"
+import axios from 'axios'
+import { Container, Row, Col } from "shards-react"
 
-import PageTitle from "./../components/common/PageTitle";
+import PageTitle from "./../components/common/PageTitle"
 import CountOverview from "../components/overview/CountOverview"
-import DrivingHistory from "./../components/overview/DrivingHistory";
+import DrivingHistory from "./../components/overview/DrivingHistory"
 
-const Overview = (props) => (
-  <Container fluid className="main-content-container px-4 pb-4">
-    {/* Page Header */}
-    <Row noGutters className="page-header py-4">
-      <PageTitle title="현재 승하차 인원" subtitle="Busvisor" className="text-sm-left mb-3" />
-    </Row>
+class Overview extends React.Component {
+  state = {
+    dr_status: -1,
+    dr_cnt: 5
+  }
+  
+  getStatus = (url, cb) => {
+    axios.get(url)
+      .then(res => {
+        cb(res.data);
+      })
+      .catch(e => console.log(e));
+  }
 
-    <Row>
-      <Col lg="8" md="12" sm="12" className="mb-4">
-        <CountOverview />
-      </Col>
+  handleChangeState = (data) => {
+    this.setState({
+      dr_status: data.dr_type,
+      dr_cnt: data.cnt
+    })
+  }
 
-      <Col  lg="4" md="12" sm="12" className="mb-4">
-        <DrivingHistory />
-      </Col>
-    </Row>
+  componentDidMount() {
+    const url = 'http://52.231.67.172:8088/api/device/status/sc13150';
 
-    
-  </Container>
-);
+    this.getStatus(url, this.handleChangeState);
+    this.timer = setInterval(this.getStatus, 1000, url, this.handleChangeState);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  render() {
+    return(
+      <Container fluid className="main-content-container px-4 pb-4">
+        {/* Page Header */}
+        <Row noGutters className="page-header py-4">
+          <PageTitle title="현재 승하차 인원" subtitle="Busvisor" className="text-sm-left mb-3" />
+        </Row>
+
+        <Row>
+          <Col lg="8" md="12" sm="12" className="mb-4">
+            <CountOverview cnt={this.state.dr_cnt} dr_status={this.state.dr_status}/>
+          </Col>
+
+          <Col  lg="4" md="12" sm="12" className="mb-4">
+            <DrivingHistory />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
 
 export default Overview;
